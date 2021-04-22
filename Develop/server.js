e1dc821c6af0b6b070db14c41ed3a7efea7cf902
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-04-22 14:36:52 -0400

    Set up ports and routes on server.js

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+// set up global variables for server.
+const express = require('express');
+const path = require('path');
+const app = express();
+// set up PORT for server hosting, if not available localhost 8000 will be used
+const PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//HTML Routes
+
+app.get('/notes',(req,res)=>res.sendFile(path.join(__dirname,'./public/notes.html')));
+app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'./public/index.html')));
+
+/* When loading server using a '*' route it will default to index.html this needs to be stacked last
+or else other routes will not work */
+
+// code to start the server.
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
