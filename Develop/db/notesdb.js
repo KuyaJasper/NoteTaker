c765efd7155a1f236320adba312c55d571c3a771
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-04-22 15:10:09 -0400

    moved db json into nodesdb.js. added path to server.js

---
+++ Develop/db/notesdb.js
@@ -0,0 +1,11 @@
+// moved db.json into js file to export easier,
+
+const DBarray = [
+    {
+        "title":"Test Title",
+        "text":"Test text"
+    }
+];
+
+module.exports = DBarray;
+
--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const DbArray = require('./db/notesdb');
 
 // set up PORT for server hosting, if not available localhost 8000 will be used
 const PORT = process.env.PORT || 8000;
@@ -16,6 +17,18 @@ app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'./public/index.html')))
 
 /* When loading server using a '*' route it will default to index.html this needs to be stacked last
 or else other routes will not work */
+
+// API get data
+
+app.get('api/notes',(req,res)=> res.json(DbArray));
+
+//API post data
+
+app.post('api/notes',(req,res) =>{
+    const newNote = req.body;
+    DbArray.push(newNote);
+    res.json(true);
+})
 
 // code to start the server.
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
