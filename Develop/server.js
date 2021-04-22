11d1a9bc1efa826e025868165908014bb76c85e1
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-04-22 16:09:08 -0400

    added.static to display json objects

--- Develop/server.js
+++ Develop/server.js
@@ -7,16 +7,13 @@ const NotesDB = require('./db/notesdb');
 // set up PORT for server hosting, if not available localhost 8000 will be used
 const PORT = process.env.PORT || 8000;
 
+//HTML Routes
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-
-//HTML Routes
+// static makes the server and html talk
+app.use(express.static("public"));
 
 app.get('/notes',(req,res)=> res.sendFile(path.join(__dirname,'./public/notes.html')));
-app.get('*',(req,res)=> res.sendFile(path.join(__dirname,'./public/index.html')));
-
-/* When loading server using a '*' route it will default to index.html this needs to be stacked last
-or else other routes will not work */
 
 // API get data
 
@@ -29,6 +26,11 @@ app.post('/api/notes',(req,res) =>{
     NotesDB.push(newNote);
     res.json(true);
 })
+
+app.get('/*',(req,res)=> res.sendFile(path.join(__dirname,'./public/index.html')));
+
+/* When loading server using a '*' route it will default to index.html this needs to be stacked last
+or else other routes will not work */
 
 // code to start the server.
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
