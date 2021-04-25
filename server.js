b247049785ced3170b6b4d74eb13b1f978f32531
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-04-24 20:43:22 -0400

    Set up app.delete

--- server.js
+++ server.js
@@ -1,36 +1,51 @@
 // Global variables for server.
 const express = require('express');
-const { fstat } = require('fs');
+const fs = require('fs');
 const path = require('path');
 const app = express();
-const { v4: uuid} = require('uuid');
+const { v4: uuid } = require('uuid');
 const NotesDB = require('./db/db.json');
+
+
 // set up PORT for server hosting, if not available localhost 8000 will be used
 const PORT = process.env.PORT || 8000;
 
 //HTML Routes
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // static makes the server and html talk
 app.use(express.static("public"));
 
-app.get('/notes',(req,res)=> res.sendFile(path.join(__dirname,'./public/notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 // API get data
 
-app.get('/api/notes',(req,res)=> res.json(NotesDB));
+app.get('/api/notes', (req, res) => res.json(NotesDB));
 
 //API post data
 
 app.post('/api/notes',(req,res) =>{
-    let id = uuid();
+    let idNumber = uuid();
     let newNote = req.body;
-    newNote.id = id;
+    newNote.id = idNumber;
     NotesDB.push(newNote);
-    res.json(true);
-})
+    res.json(NotesDB);
+});
 
-app.get('/*',(req,res)=> res.sendFile(path.join(__dirname,'./public/index.html')));
+
+//Delete post
+app.delete('/api/notes/:id', (req, res) => {
+    let noteList = JSON.parse(fs.readFileSync('./db/db.json'));
+    noteList = noteList.filter((note) => {
+        return note.id != req.params.id
+    });
+    fs.writeFile('./db/db.json', JSON.stringify(noteList), () => {
+        res.json(noteList);
+    });
+});
+
+
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 /* When loading server using a '*' route it will default to index.html this needs to be stacked last
 or else other routes will not work */
